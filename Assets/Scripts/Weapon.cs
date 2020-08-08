362207ffcda9ffa7aac6b8b33675bb62fc6fe321
Matěj Beran <matej.beran96@gmail.com>
2020-08-08 11:01:14 +0200

    Add shooting via raycasting.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        bool isHit = Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out RaycastHit hit, range);
+        if (isHit)
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
