40edff6972c9b885183c0aa725783d6eff483ad9
Matěj Beran <matej.beran96@gmail.com>
2020-08-12 15:45:16 +0200

    Create zoom in/out functionality.

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFov = 30f;
+
+    bool zoomedIn = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedIn)
+            {
+                fpsCamera.fieldOfView = zoomedOutFOV;
+            }
+            else
+            {
+                fpsCamera.fieldOfView = zoomedInFov;
+            }
+
+            zoomedIn = !zoomedIn;
+        }
+    }
+}
