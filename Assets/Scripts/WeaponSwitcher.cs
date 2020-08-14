4ca2b266b41b255fe3a9107708f25f4afc73cb27
Matěj Beran <matej.beran96@gmail.com>
2020-08-14 18:46:20 +0200

    Set active weapon.

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    [SerializeField] int currentWeapon = 0;
+
+    void Start()
+    {
+        SetWeaponActive();
+    }
+
+    private void SetWeaponActive()
+    {
+        int weaponIndex = 0;
+        foreach (Transform weapon in transform)
+        {
+            weapon.gameObject.SetActive(weaponIndex++ == currentWeapon);
+        }
+    }
+}
